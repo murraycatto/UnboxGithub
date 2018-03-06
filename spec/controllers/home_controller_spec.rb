d2bf26c68197bd59099d6dbf35548367ba47ad2c
Murray Catto <murraycatto@live.co.za>
2018-03-06 18:22:15 +0200

    create failing test for index action of home page

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
 
+  it "renders the home page" do
+    get :index
+    expect(response).to render_template(:index)
+  end
 end
